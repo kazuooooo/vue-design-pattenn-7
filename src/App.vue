474d3f089cd36b86cc3dad51dccde2b60d5be14d
kazuwombat <matsumotokazuya7@gmail.com>
2022-06-30 01:08:04 +0900

    7-3

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <renderless-password
     :password="input.password"
     :confirmation="input.confirmation"
-    v-slot="{ matching }"
+    v-slot="{ matching, complexity }"
   >
     <div class="wrapper">
       <div class="field">
@@ -13,8 +13,16 @@
         <label for="confirmation">Confirmation</label>
         <input v-model="input.confirmation" id="confirmation" />
       </div>
+      <div class="complexity-field">
+        <div
+          role="password-complexity"
+          class="complexity"
+          :class="complexityStyle(complexity)"
+        />
+      </div>
     </div>
     <p>Matches: {{ matching }}</p>
+    <p>Complexity: {{ complexity }}</p>
   </renderless-password>
 </template>
 
@@ -31,9 +39,48 @@ export default defineComponent({
       password: "",
       confirmation: "",
     })
+
+    const complexityStyle = (complexity: number) => {
+      if (complexity >= 3) {
+        return "high"
+      }
+      if (complexity >= 2) {
+        return "mid"
+      }
+      if (complexity >= 1) {
+        return "low"
+      }
+    }
+
     return {
       input,
+      complexityStyle,
     }
   },
 })
 </script>
+<style scoped>
+.complexity-field {
+  width: 100%;
+}
+
+.complexity {
+  transition: 0.2s;
+  height: 10px;
+}
+
+.high {
+  width: 100%;
+  background: lime;
+}
+
+.mid {
+  width: 66%;
+  background: yellow;
+}
+
+.low {
+  width: 33%;
+  background: red;
+}
+</style>
--- src/components/RenderlessPassword.vue
+++ src/components/RenderlessPassword.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import { computed, defineComponent } from "vue"
 
-export const isMatching = (
+const isMatching = (
   password: string | undefined,
   confirmation: string | undefined
 ) => {
@@ -9,6 +9,22 @@ export const isMatching = (
     return false
   }
   return password === confirmation
+}
+
+const calcComplexity = (val: string | undefined) => {
+  if (!val) {
+    return 0
+  }
+  if (val.length >= 10) {
+    return 3
+  }
+  if (val.length >= 7) {
+    return 2
+  }
+  if (val.length >= 5) {
+    return 1
+  }
+  return 0
 }
 
 export default defineComponent({
@@ -24,10 +40,12 @@ export default defineComponent({
     const matching = computed(() =>
       isMatching(props.password, props.confirmation)
     )
+    const complexity = computed(() => calcComplexity(props.password))
 
     return () =>
       slots.default!({
         matching: matching.value,
+        complexity: complexity.value,
       })
   },
 })
